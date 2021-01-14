8dc5ace621208be274c9c610bbaa1b398c3e90da
LEBOURGEOIS Jacques IMT/OLS <jacques.lebourgeois@orange.com>
2021-01-14 18:47:00 +0100

    init project

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from '../node_modules/iotmapmanager/iotMapClusters';
+export * from '../node_modules/iotmapmanager/iotMapManager';
+export * from '../node_modules/iotmapmanager/iotMapManagerConfig';
+export * from '../node_modules/iotmapmanager/iotMapManagerTypes';
+export * from '../node_modules/iotmapmanager/iotMapMarkers';
+export * from '../node_modules/iotmapmanager/iotMapUserMarkers';
+export * from '../node_modules/iotmapmanager/iotMapClusters';
